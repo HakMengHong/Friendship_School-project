f2e66711e0603cef23194544172c2edf966821f4
HakMengHong <67801410+HakMengHong@users.noreply.github.com>
2025-07-03 10:19:43 +0700

    Updated page.tsx

--- app/subjects/page.tsx
+++ app/subjects/page.tsx
@@ -1,5 +1,4 @@
 'use client'
-export const dynamic = "force-dynamic"
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { BookOpen, Users, Clock, Award } from "lucide-react"
