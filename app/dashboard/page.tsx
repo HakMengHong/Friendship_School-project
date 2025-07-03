06015d9a3277025f36a60c8f76e4cd9c594b370c
HakMengHong <67801410+HakMengHong@users.noreply.github.com>
2025-07-03 10:35:30 +0700

    Updated page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -67,6 +67,7 @@ export default function DashboardPage() {
   };
 
   return (
+    <>
     <div className="space-y-8">
       <div>
         <h2 className="text-3xl font-bold text-primary mb-2">សូមស្វាគមន៍មកកាន់ផ្ទាំងគ្រប់គ្រង</h2>
@@ -347,5 +348,6 @@ export default function DashboardPage() {
         </div>
       </div>
     </div>
+      </>
   )
 }
