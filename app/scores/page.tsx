8cbef4302b0edea0ad61656bfba8a954c82402c3
HakMengHong <67801410+HakMengHong@users.noreply.github.com>
2025-07-03 10:19:17 +0700

    Updated page.tsx

--- app/scores/page.tsx
+++ app/scores/page.tsx
@@ -1,5 +1,4 @@
 'use client'
-export const dynamic = "force-dynamic"
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { BarChart2, TrendingUp, Award, Target, Medal } from "lucide-react"
