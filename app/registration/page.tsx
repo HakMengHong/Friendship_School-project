9dfd6a2f6557b43d3b8c8237b6b7baf9ebb384f6
HakMengHong <67801410+HakMengHong@users.noreply.github.com>
2025-07-03 10:19:03 +0700

    Updated page.tsx

--- app/registration/page.tsx
+++ app/registration/page.tsx
@@ -1,5 +1,4 @@
 'use client'
-export const dynamic = "force-dynamic"
 
 import { useState } from "react"
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card"
